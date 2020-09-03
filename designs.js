0a759d7e363c2df042123b14c1fe55b858b1c2c6
Chantelle Reynolds <70683104+ChantyR@users.noreply.github.com>
2020-09-03 18:40:39 -0400

    Add files via upload

--- designs.js
+++ designs.js
@@ -11,26 +11,32 @@ const pixelTable = document.querySelector('#pixelCanvas');
 mySubmit.addEventListener('click', makeGrid);
 
 function respondToPixelArt() {
-    event.target.setAttribute('style', 'background-color:' + gridColor.value);
-    //place code to change background color here
+    if(event.target.nodeName === 'TD') {
+        event.target.setAttribute('style', 'background-color:' + gridColor.value); //Changes the background color of targeted cell within pixelTable
+    }
 }
 
 function makeGrid () {
-    while (pixelTable.firstChild) { //clears the grid from any previous input
+    while (pixelTable.firstChild) { //Checks if pixelTable has any children (grid cells) inside it. If it does, it removes it and then runs this code again.
         pixelTable.removeChild(pixelTable.firstChild);
     }
 
     const gridHeight = document.querySelector('#inputHeight');
     const gridWidth = document.querySelector('#inputWidth');
-    for (let height = 1; height <= gridHeight.value; height ++) {
-        const newRow = document.createElement('tr'); //Create an entire row
-        for (let width = 1; width <= gridWidth.value; width ++) {
-            const newColumn = document.createElement('td'); //Create individual cells within above row
+    if (gridHeight.value > 100 || gridWidth.value > 100) {
+        gridHeight.value = 1
+        gridWidth.value= 1
+    }
+    for (let height = 1; height <= gridHeight.value; height ++) { //Sets a loop to begin creating the grid cells. This loop corresponds to the the height of the user-inputted grid.
+        const newRow = document.createElement('tr'); //Create an entire row of cells
+        for (let width = 1; width <= gridWidth.value; width ++) { //Sets an inner loop to create cells equal to the width of the user-inputted grid.
+            const newColumn = document.createElement('td'); //Create individual cells within the above row
             newRow.appendChild(newColumn);
         }
         pixelTable.appendChild(newRow);
     }
-    pixelTable.addEventListener('click', respondToPixelArt);
+    pixelTable.addEventListener('click', respondToPixelArt); //Adds a listener to the pixelTable id. 
+    
 
     
 }
